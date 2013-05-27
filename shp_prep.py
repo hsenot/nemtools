dc64707a13381d6fa35ce9dc84de2a9d9cad2026
Herve Senot <hsenot@gmail.com>
2013-05-27 15:48:04 +1000

    Auto-snapping and identification of intersections

--- shp_prep.py
+++ shp_prep.py
@@ -38,21 +38,60 @@ try:
 	cur.execute(sql)
 	conn.commit()
 
-	# Recreating minified table
+	# Recreating minified table as a grid-snapped version
 	print "Step 2"
 	sql = "CREATE TABLE "+table_out+"_mini AS SELECT gid,geom as the_geom FROM "+table_out
 	print sql
 	cur.execute(sql)
 	conn.commit()
 
-	# Truncating it
+	# Getting the IDs (gids) of the 
 	print "Step 3"
-	sql = "TRUNCATE TABLE "+table_out+"_mini"
+	sql = "SELECT gid from "+table_out+"_mini"
+	print sql
+	cur.execute(sql)
+	rows = cur.fetchall()
+
+	# Snapping every route to every other route - to avoid duplicate / slightly different routes along the same road (ex: other side of the road, ...)
+	exceptions = [['6','15']]
+	for row in rows:
+		print "Step 4x"
+		sql = "UPDATE "+table_out+"_mini SET the_geom=(select ST_Snap("+table_out+"_mini.the_geom,a.the_geom,0.00011) from "+table_out+"_mini a where a.gid = "+str(row[0])+")"
+		print sql
+		cur.execute(sql)
+		conn.commit()
+
+	# Manual re-snapping
+	resnap = [['13','2','0.0005'],['2','5','0.0005'],['4','5','0.0005'],['15','4','0.0005'],['16','5','0.005'],['17','6','0.0001'],['16','15','0.0005'],['7','1','0.0005'],['7','5','0.0003'],['10','9','0.0002'],['3','9','0.0005']]
+	for val in resnap:
+		print "Step 5x"
+		sql = "UPDATE "+table_out+"_mini SET the_geom=(select ST_Snap("+table_out+"_mini.the_geom,a.the_geom,"+str(val[2])+") from "+table_out+"_mini a where a.gid = "+str(val[1])+") where gid="+str(val[0])
+		print sql
+		cur.execute(sql)
+		conn.commit()
+
+
+	# Dropping minified table
+	print "Step 5"
+	sql = "DROP TABLE IF EXISTS "+table_out+"_inter CASCADE;"
+	print sql
+	cur.execute(sql)
+	conn.commit()
+
+	# Creating a point structure based on the intersections - dimension 0 intersections
+	print "Step 6"
+	sql = "create table "+table_out+"_inter as select * from (select cast(nextval('"+table_out+"_gid_seq') as integer) AS id,(ST_Dump(ST_Intersection(a.the_geom,b.the_geom))).geom as the_geom from "+table_out+"_mini a, "+table_out+"_mini b where a.gid < b.gid and ST_Intersects(a.the_geom,b.the_geom)) t where st_dimension(the_geom)=0"
 	print sql
 	cur.execute(sql)	
 	conn.commit()
 
-	# Now, the fun begins - let's cut the spaghetti
+	# Inserting in the point structure the intersections - dimension 1 intersections
+	print "Step 7"
+	sql = "insert into "+table_out+"_inter select id,(st_dump(ST_Collect(ST_startpoint(the_geom),st_endpoint(the_geom)))).geom as the_geom from (select cast(nextval('"+table_out+"_gid_seq') as integer) AS id,(ST_Dump(ST_LineMerge(ST_Intersection(a.the_geom,b.the_geom)))).geom as the_geom from "+table_out+"_mini a, "+table_out+"_mini b where a.gid < b.gid and ST_Intersects(a.the_geom,b.the_geom)) t where st_dimension(the_geom)=1"
+	print sql
+	cur.execute(sql)	
+	conn.commit()
+
 
 	# Determining intersection points
 	# Using an interative algorithm, considering all existing segments one after the other (loop)
