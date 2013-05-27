3b54dfad65c15acf386779da1f5984d5752c7078
Herve Senot <hsenot@gmail.com>
2013-05-27 18:28:05 +1000

    Added the tram GTFS translation

--- gtfs2kml.py
+++ gtfs2kml.py
@@ -3,12 +3,18 @@ import psycopg2
 
 gtfs_in = "in/gtfs/train-20120802.zip"
 filename_out = "shp_from_gtfs-train"
-target_srid = str(4326)
 
-# Now playing with the created table
+gtfs2_in = "in/gtfs/tram-20120802.zip"
+filename2_out = "shp_from_gtfs-tram"
+
 try:
 	# Creating a KML file from the GTFS file
 	cmd = "python gtfs/python/kmlwriter.py "+gtfs_in+" tmp/"+filename_out+".kml"
+	print "Executing command: "+cmd
+	os.system(cmd)
+
+	# Creating a KML file from the GTFS file
+	cmd = "python gtfs/python/kmlwriter.py "+gtfs2_in+" tmp/"+filename2_out+".kml"
 	print "Executing command: "+cmd
 	os.system(cmd)
 
