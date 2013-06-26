f8c464c441f7f992eb38fa85c429d20ff0f5c3e9
Herve Senot <hsenot@gmail.com>
2013-06-26 13:29:03 +1000

    Added stop dataset, Kanzeit and Laenge sections

--- shp_check.py
+++ shp_check.py
@@ -1,11 +1,16 @@
 import os
 import psycopg2
 
+
 shp_line_in  = "out/network-self-snapped-reworked.shp"
 shp_point_in = "out/interchange.shp"
+shp_stop_in = "out/stop.shp"
+
+nem_filename_out = "out/new_network.nem"
 
 table_line_out = "nw_line"
 table_point_out = "nw_point"
+table_stop_out = "nw_stop"
 
 target_srid = str(4326)
 db_name = "bze"
@@ -35,6 +40,18 @@ cmd = "psql -p "+db_port+" -d "+db_name+" -f "+table_point_out+".sql"
 print "Executing command: "+cmd
 os.system(cmd)
 
+print "Preparing the shapefile "+shp_stop_in+" ..."
+
+# Loading the line shapefile into the database
+cmd = "shp2pgsql -W LATIN1 -d -D -I -t 2D -S -s "+target_srid+" "+shp_stop_in+" "+table_stop_out+" > "+table_stop_out+".sql"
+print "Executing command: "+cmd
+os.system(cmd)
+
+# Loading the result using psql
+cmd = "psql -p "+db_port+" -d "+db_name+" -f "+table_stop_out+".sql"
+print "Executing command: "+cmd
+os.system(cmd)
+
 
 # Now playing with the loaded table
 try:
@@ -51,16 +68,29 @@ try:
 	# The game here is to detect if all lines can be decomposed as successions of nodes
 	# And to help perform visual checks to make sure all stops are well and trully connected to the lines
 
+	# Merging points from the stop dataset into the nw_point
+	print "Step 1a"
+	sql = "update nw_point set typ='INTERSECT';"
+	print sql
+	cur.execute(sql)
+	conn.commit()
+
+	# Merging points from the stop dataset into the nw_point
+	print "Step 1b"
+	sql = "insert into nw_point (typ,geom) select 'LINEEND',geom from nw_stop"
+	print sql
+	cur.execute(sql)
+	conn.commit()
 
 	# Setting the IDs of points right, so that we are sure they have unique IDs
-	print "Step 1"
+	print "Step 1c"
 	sql = "update nw_point set id=gid;"
 	print sql
 	cur.execute(sql)
 	conn.commit()
 
 	# Exporting a NEM file based on the lines and points identified
-	fo = open("test.nem", "w+")
+	fo = open(nem_filename_out, "w+")
 
 	# Writing the header
 	fo.write(
@@ -74,8 +104,8 @@ Meterpropixel;111
 Koordinatensystem;0;0;1;1
 Koordinatentyp;Kugel
 
-Startmittelpunkt;133.8981677;-25.50912931
-Startzoomwert;1346.2;1E-5
+Startmittelpunkt;145.0869274;-37.88879178
+Startzoomwert;60179;1E-5
 Tortenfaktor;6.879E-6
 Zeichenmodus;3
 Tortendarstellung;1
@@ -99,8 +129,8 @@ Language;English
 	cur.execute(sql)
 	rows = cur.fetchall()
 
+	print "Step 2a"
 	for row in rows:
-		print "Step 2a"
 		print "Point ID: "+str(row[0])
 		fo.write("Punkt;X"+str(row[0])+";"+str(row[1])+";"+str(row[2])+";Intersection "+str(row[0])+";K;0;0;300;300")
 		fo.write("\n")
@@ -128,45 +158,99 @@ Language;English
 	cur.execute(sql)
 	rows = cur.fetchall()
 
+	print "Step 3a"
 	for row in rows:
-		print "Step 3a"
 		print "Line: "+str(row[0])
 		fo.write("Linie;"+str(row[0])+";A;1;rot;0;;;\n")
 		fo.write("Verlauf;"+str(row[0])+";"+str(row[1])+";\n")
 		fo.write("Halte;"+str(row[0])+";"+str(row[1]))
 		fo.write("\n\n")
 
-
 	# Kanzeit section - time travel = length by average speed
 	print "Step 4"
 	sql = """
-		select all pairs of consecutive stops, and the associated physical length, to be multiplied by an average speed
+select l_name,'X'||pt_a as pt_a,'X'||pt_b as pt_b,
+round(st_length(
+  st_transform(
+    st_line_substring(
+      nl.geom,
+      ST_Line_Locate_point(nl.geom,ST_ClosestPoint(nl.geom,pa.geom)),
+      ST_Line_Locate_point(nl.geom,ST_ClosestPoint(nl.geom,pb.geom))
+    )
+  ,3111)
+)::numeric,0) as leg_length 
+from
+(
+  select l_name,pt_arr[i] as pt_a,pt_arr[i+1] as pt_b 
+  from
+  (
+    select l_name,pt_arr,generate_series(1,array_length(pt_arr,1)-1) as i 
+    from
+    (
+      select l_name,array_agg(p_id) as pt_arr
+      from
+        (
+          select l.name as l_name,p.id as p_id 
+          from nw_line l, nw_point p
+          where ST_Distance(p.geom,l.geom)<0.000001
+          order by l.name,ST_Line_Locate_point(l.geom,ST_ClosestPoint(l.geom,p.geom))
+        ) t
+      group by l_name order by l_name
+    ) t
+  ) s
+) u, nw_point pa, nw_point pb, nw_line nl
+where pa.id=u.pt_a and pb.id=u.pt_b
+and ST_Distance(pa.geom,nl.geom)<0.000001
+and ST_Distance(pb.geom,nl.geom)<0.000001
+and nl.name=u.l_name order by l_name
 	"""
 	print sql
 	cur.execute(sql)
 	rows = cur.fetchall()
 
+	print "Step 4a"
 	for row in rows:
-		print "Step 4a"
-		print "Edge: "+str(row[0])
-		fo.write("Kanzeit;"+str(row[0])+";"+str(row[1])+";A;"+str(row[2])+"\n")
+		print "Edge: "+str(row[1])+" to "+str(row[2])+" on line "+str(row[0])
 
-	fo.close()
+		# Applying an average speed to the segment length
+		# 
+		avg_speed_km_per_hour = 20
+		duration_in_sec = int(round(int(row[3])/(avg_speed_km_per_hour*1000.0/3600),0))
+		fo.write("Kanzeit;"+str(row[1])+";"+str(row[2])+";A;"+str(duration_in_sec)+"\n")
+
+
+	fo.write("\n\n")
 
 	# Laegen section: physical length of each edge
-	# Very close to previous query
+	print "Step 5"
+	for row in rows:
+		print "Edge: "+str(row[1])+" to "+str(row[2])+" on line "+str(row[0])
 
+		# Segment length is already in the 4th attribute
+		fo.write("Laenge;"+str(row[1])+";"+str(row[2])+";A;"+str(row[3])+"\n")
+
+	fo.write("\n\n")
+	fo.close()
+
+	"""
 	# Exporting the line table as a shapefile
 	print "Step 6a"	
-	cmd = "pgsql2shp -f "+shp_line_in+" -p "+db_port+" "+db_name+" "+table_line_out
+	cmd = "pgsql2shp -f "+shp_line_in2+" -p "+db_port+" "+db_name+" "+table_line_out
 	print "Executing command: "+cmd
 	#os.system(cmd)	
 	
 	# Exporting the point table as a shapefile
 	print "Step 6b"	
-	cmd = "pgsql2shp -f "+shp_point_in+" -p "+db_port+" "+db_name+" "+table_point_out
+	cmd = "pgsql2shp -f "+shp_point_in2+" -p "+db_port+" "+db_name+" \"SELECT id,geom FROM nw_point WHERE typ='INTERSECT'\""
 	print "Executing command: "+cmd
 	#os.system(cmd)	
+
+	# Exporting the stop table as a shapefile
+	print "Step 6b"	
+	cmd = "pgsql2shp -f "+shp_stop_in2+" -p "+db_port+" "+db_name+" \"SELECT id,geom FROM nw_point WHERE typ='LINEEND'\""
+	print "Executing command: "+cmd
+	#os.system(cmd)	
+	"""
 
 except Exception,e: 
     print "I can't do that"
