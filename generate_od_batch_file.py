27977f53066d58a2a97b6566dc5f49c9b7b17f1f
Herve Senot <hsenot@gmail.com>
2013-07-10 16:57:26 +1000

    Exclude ODs that are not connected

--- generate_od_batch_file.py
+++ generate_od_batch_file.py
@@ -1,11 +1,18 @@
 import os
 import psycopg2
 
+# Value under which an OD pair will not be written to the batch file to submit to Netview
+# i.e. minimum traffic volume for an OD pair to be considered
+weight_threshold = 5.0
+
+# Files, in and out
 d1 = "C:\Herve\data\od_csv\PT_Demand_Base.csv"
 d2 = "C:\Herve\data\od_csv\PVV_DEMAND_Base.csv"
+
+# still to do: write several OD CSVs (batches of X OD pairs i.e. OD=10,000)
 od_csv_file = "out/od.csv"
 
-# Other variables
+# Database variables
 db_name = "bze"
 db_port = str(54321)
 
@@ -20,6 +27,26 @@ except:
     print "I am unable to connect to the database"
 
 cur = conn.cursor()
+# retrieving a dictionary of OD points that are disconnected from the network so they should not be allowed in the Netview OD routing batch file
+print "Step 1"
+sql = """
+select gid from nw_od_point odp where odp.geom not in 
+(
+select ST_StartPoint(odl.geom) from nw_od_line odl
+union
+select ST_EndPoint(odl.geom) from nw_od_line odl
+);"""
+print sql
+cur.execute(sql)
+rows = cur.fetchall()
+
+od_pt_to_exclude = {}
+
+print "Step 1a"
+for row in rows:
+	print "OD point to exclude: "+str(row[0])
+	od_pt_to_exclude["E"+str(row[0])]=True
+
 # Exporting a NEM file based on the lines and points identified
 fo = open(od_csv_file, "w+")
 
@@ -55,9 +82,13 @@ with open(d2,'r') as f2:
 
 print "Writing OD CSV file ..."
 for o in big_dict.keys():
-	for d in big_dict[o].keys():
-		if float(big_dict[o][d])>1.0:
-			fo.write("E"+str(o)+";E"+str(d)+";From E"+str(o)+";To E"+str(d)+";1;1\n")
+	# If the OD has been flagged as "to exclude" we do nothing
+	if not od_pt_to_exclude.has_key("E"+str(o)):
+		for d in big_dict[o].keys():
+			# If the OD has been flagged as "to exclude" we do nothing
+			if not od_pt_to_exclude.has_key("E"+str(d)):
+				if float(big_dict[o][d])>weight_threshold:
+					fo.write("E"+str(o)+";E"+str(d)+";From E"+str(o)+";To E"+str(d)+";1;1\n")
 
 fo.write("\n")
 fo.close()
